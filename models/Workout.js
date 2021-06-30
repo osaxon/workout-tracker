3b93b6921a254025820f2d2a9387d51e08a03c64
Oliver Saxon <oliverrsaxon@gmail.com>
2021-06-30 21:59:13 +0100

    Added index for model

--- models/Workout.js
+++ models/Workout.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    date: {
+    day: {
         type: Date,
         default: Date.now
     },
-    exercise: [
+    exercises: [
         {
             type: {
                 type: String
