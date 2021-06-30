6f0deb87fe67b5378515de617757bb5956885c9b
Oliver Saxon <oliverrsaxon@gmail.com>
2021-06-30 21:52:45 +0100

    Workout model defined

--- models/Workout.js
+++ models/Workout.js
@@ -2,10 +2,41 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    day: {
+    date: {
         type: Date,
         default: Date.now
     },
+    exercise: [
+        {
+            type: {
+                type: String
+            },
+
+            name: {
+                type: String
+            },
+
+            weight: {
+                type: Number
+            },
+            
+            sets: {
+                type: Number
+            },
+
+            reps: {
+                type: Number
+            },
+
+            duration: {
+                type: Number
+            },
+
+            distance: {
+                type: Number,
+            }
+        }
+    ]
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
