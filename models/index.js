0ab1a181b00a06aebafd2a89ffcd159d68338d5f
Oliver Saxon <oliverrsaxon@gmail.com>
2021-06-30 22:42:30 +0100

    Fixed issue where data did not return from api request

--- models/index.js
+++ models/index.js
@@ -1,5 +1,3 @@
-const Workout = require('./Workout');
-
 module.exports = {
-    Workout,
+    Workout: require('./Workout.js')
 }
--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,18 @@
 const router = require('express').Router();
 const path = require('path');
+const db = require('../models');
+
+router.get('/api/workouts/range', (req, res) => {
+    db.Workout.find({})
+    .then((data) => {
+        console.log(data)
+        res.status(200).json(data)
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    })
+    
+})
 
 
 module.exports = router;
