153268397c650b5f6a7c4de18b79dd4c4467f28e
Oliver Saxon <oliverrsaxon@gmail.com>
2021-06-30 15:37:36 +0100

    Workout schema initialised

---
+++ models/Workout.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+module.exports = Workout;
